6323554f5f208afd05dca7f39cb5b6dc9e7bd097
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-13 01:57:36 +0100

    DropShadow
    DropShadow I guess

---
+++ TacoPopJack/DropShadow.swift
@@ -0,0 +1,21 @@
+//
+//  DropShadow.swift
+//  TacoPopJack
+//
+//  Created by MACBOOK on 13/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {}
+
+//1st thing he did was to apply effects on the header
+extension DropShadow where Self: UIView {
+    func addDropShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 8
+    }
+}
