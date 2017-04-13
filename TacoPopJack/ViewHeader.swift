c10fb80895a7a846407cf0e24cde854fbce1b95a
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-13 02:01:55 +0100

    ViewHeader
    //2nd conform to protocol extension

---
+++ TacoPopJack/ViewHeader.swift
@@ -0,0 +1,12 @@
+//
+//  ViewHeader.swift
+//  TacoPopJack
+//
+//  Created by MACBOOK on 13/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import UIKit
+
+//2nd conform to protocol extension
+class ViewHeader: UIView, DropShadow { }
